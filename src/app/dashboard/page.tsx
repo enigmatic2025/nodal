1e9232b962227508b0cb77764fe4adc01a04e039
Sam Tran <htsam22@gmail.com>
2025-05-08 09:00:45 -0600

    status

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,22 +7,22 @@ export default function Dashboard() {
             <div className="pt-5 pb-5">
                 <h2 className="text-lg font-semibold mb-3">Quick Access</h2>
                 <div className="w-100 grid grid-cols-2 gap-5">
-                    <div className="flex flex-col hover:shadow-sm h-50 w-50 border-[0.5px] border-black/20 rounded-md cursor-pointer p-5">
+                    <div className="flex flex-col hover:shadow-md h-50 w-50 border-[0.5px] border-black/20 rounded-md cursor-pointer p-5">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#6581FF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-package-plus-icon lucide-package-plus"><path d="M16 16h6" /><path d="M19 13v6" /><path d="M21 10V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l2-1.14" /><path d="m7.5 4.27 9 5.15" /><polyline points="3.29 7 12 12 20.71 7" /><line x1="12" x2="12" y1="22" y2="12" /></svg>
                         <p className="text-[1.1rem] font-semibold mt-auto">Create Action</p>
                         <p className="h-[3lh] text-[0.8rem] font-normal text-gray-500">Build a custom action for an individual, team, or department.</p>
                     </div>
-                    <div className="flex flex-col hover:shadow-sm h-50 w-50 border-[0.5px] border-black/20 rounded-md cursor-pointer p-5">
+                    <div className="flex flex-col hover:shadow-md h-50 w-50 border-[0.5px] border-black/20 rounded-md cursor-pointer p-5">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#6581FF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-download-icon lucide-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline points="7 10 12 15 17 10" /><line x1="12" x2="12" y1="15" y2="3" /></svg>
                         <p className="text-[1.1rem] font-semibold mt-auto">Export Data</p>
                         <p className="h-[3lh] text-[0.8rem] font-normal text-gray-500">Generate a file containing your data, ready for download.</p>
                     </div>
-                    <div className="flex flex-col hover:shadow-sm h-50 w-50 border-[0.5px] border-black/20 rounded-md cursor-pointer p-5">
+                    <div className="flex flex-col hover:shadow-md h-50 w-50 border-[0.5px] border-black/20 rounded-md cursor-pointer p-5">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#6581FF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-blocks-icon lucide-blocks"><rect width="7" height="7" x="14" y="3" rx="1" /><path d="M10 21V8a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H3" /></svg>
                         <p className="text-[1.1rem] font-semibold mt-auto">Action Pool</p>
                         <p className="h-[3lh] text-[0.8rem] font-normal text-gray-500">Openly unassigned actions. Help your colleagues or seek help.</p>
                     </div>
-                    <div className="flex flex-col hover:shadow-sm h-50 w-50 border-[0.5px] border-black/20 rounded-md cursor-pointer p-5">
+                    <div className="flex flex-col hover:shadow-md h-50 w-50 border-[0.5px] border-black/20 rounded-md cursor-pointer p-5">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#6581FF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-binoculars-icon lucide-binoculars"><path d="M10 10h4" /><path d="M19 7V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v3" /><path d="M20 21a2 2 0 0 0 2-2v-3.851c0-1.39-2-2.962-2-4.829V8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v11a2 2 0 0 0 2 2z" /><path d="M 22 16 L 2 16" /><path d="M4 21a2 2 0 0 1-2-2v-3.851c0-1.39 2-2.962 2-4.829V8a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v11a2 2 0 0 1-2 2z" /><path d="M9 7V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v3" /></svg>
                         <p className="text-[1.1rem] font-semibold mt-auto">Track Process</p>
                         <p className="h-[3lh] text-[0.8rem] font-normal text-gray-500">View a specific process audit trail, initiation to completion.</p>
--- src/components/dashboard/actions-table.tsx
+++ src/components/dashboard/actions-table.tsx
@@ -3,30 +3,33 @@ import { ActionsData } from "@/data/actions-data"
 export function ActionsTable() {
     return (
         <div className="flex-grow min-h-[590px] min-w-[600px] border-[0.5px] border-black/20 rounded-md pb-5 pl-5 pr-5 overflow-auto">
-            <div className="sticky top-0 grid grid-cols-5 gap-x-5 bg-white z-10 py-3 text-gray-500 font-semibold">
-                <div>Status</div>
-                <div>Process</div>
-                <div>Description</div>
-                <div>Due Date</div>
-                <div>Audit Trail</div>
+            <div className="sticky top-0 grid grid-cols-5 gap-x-5 bg-white z-10 py-3 text-black font-semibold">
+                <div>STATUS</div>
+                <div>PROCESS</div>
+                <div>DESCRIPTION</div>
+                <div>DUE DATE</div>
+                <div>AUDIT TRAIL</div>
             </div>
             <div className="flex flex-col text-[0.9rem]">
                 {
                     ActionsData.map((item, index) => (
                         <div key={index} className="grid grid-cols-5 min-h-12 items-center gap-x-5 hover:bg-black/2 border-b-[0.5px] border-black/20 cursor-pointer">
-                            <div>
-                                <div className={`
-                                    ${item.status === "In Progress" ? "bg-blue-500" : ""}
-                                    ${item.status === "Complete" ? "bg-green-400" : ""}
-                                    ${item.status === "Cancelled" ? "bg-gray-200" : ""}
-                                    w-3
-                                    h-full
-                                    aspect-square
+                            <div className={`
+                                    ${item.status === "In Progress" ? "bg-blue-100 text-blue-500" : ""}
+                                    ${item.status === "Complete" ? "bg-green-100 text-green-500" : ""}
+                                    ${item.status === "Cancelled" ? "bg-gray-100 text-gray-500" : ""}
+                                    w-[13ch]
+                                    flex
+                                    flex-col
+                                    justify-center
+                                    items-center
                                     rounded-full
-                                    `} />
+                                    font-semibold
+                                    gap-2
+                                    `}>
                                     {item.status}
                             </div>
-                            <div className="font-semibold">
+                            <div>
                                 {item.process}
                             </div>
                             <div>
