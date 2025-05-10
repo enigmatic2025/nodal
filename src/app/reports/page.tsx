328a4552952c7c4e2751daa5514e9f745a846210
Sam Tran <htsam22@gmail.com>
2025-05-10 14:40:09 -0600

    changes

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -12,13 +12,13 @@ export default function Reports() {
                 <div className="flex flex-row gap-x-5 ml-auto pr-5">
                     <div className="group flex items-center gap-x-1 cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="group-hover:stroke-[#6581FF] lucide lucide-message-circle-question-icon lucide-message-circle-question"><path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" /><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" /><path d="M12 17h.01" /></svg>
-                        <div className="text-sm text-gray-500 group-hover:text-[#6581FF] cursor-pointer">
+                        <div className="text-xs text-gray-500 group-hover:text-[#6581FF] cursor-pointer">
                             Support
                         </div>
                     </div>
                     <div className="group flex items-center gap-x-1 cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="group-hover:stroke-[#6581FF] lucide lucide-message-square-text-icon lucide-message-square-text"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" /><path d="M13 8H7" /><path d="M17 12H7" /></svg>
-                        <div className="text-sm text-gray-500 group-hover:text-[#6581FF] cursor-pointer">
+                        <div className="text-xs text-gray-500 group-hover:text-[#6581FF] cursor-pointer">
                             Feedback
                         </div>
                     </div>
@@ -34,7 +34,7 @@ export default function Reports() {
                         bg-red-500
                         `}
                         />
-                        <div className="text-sm text-gray-500 group-hover:text-[#6581FF] cursor-pointer">
+                        <div className="text-xs text-gray-500 group-hover:text-[#6581FF] cursor-pointer">
                             Notifications
                         </div>
                     </div>
